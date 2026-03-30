5007ae8e8e9731f7907477b408e7a36164c294d6
pradeeprathod1165 <rishirathod1165@gmail.com>
2026-03-30 05:30:45 +0530

    script for variables

---
+++ variable_1.sh
@@ -0,0 +1,19 @@
+#!/bin/bash 
+#Script for variables  << single line comment
+
+<< Comment
+This is multiline comment
+Comment
+
+#variable
+name="Pradeep"
+echo "Hello $name"
+echo "$(date)"
+
+
+echo "Enter your name:"
+read username
+echo "Hello $username"
+
+
+
