ae89c7d1ddde20e2a869a003326afd198d3a57b7
pradeeprathod1165 <rishirathod1165@gmail.com>
2026-03-30 05:58:32 +0530

    Done: arguments in shell scripting

---
+++ argument_1.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+#Writing arguments here
+
+echo "Note : for arguments follow >> ./filename.sh name1 name2"
+echo "Note: name1 and name2 can be any arguments"
+
+#Example
+echo "Hello $1 and $2 you both are amazing"
+
+echo "Note: Here name1 is '$ 1' and name2 is '$ 2' and filename.sh is '$ 0'"
